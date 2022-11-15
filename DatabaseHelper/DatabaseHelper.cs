aadf602f68dab97650fcd0760b6ec259c11201f0
leandrovargasvalverde <leandro9700@gmail.com>
2022-11-14 23:12:55 -0600

    DataBase

---
+++ DatabaseHelper/DatabaseHelper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Proyecto.DatabaseHelper
+{
+    public class DatabaseHelper
+    {
+        const string servidor = @"localhost";
+        const string baseDatos = "CondiminiosProject";
+        const string strConexion = "Data Source=" + servidor + ";Initial Catalog=" + baseDatos + ";Integrated Security=True";
+
+        //Para select 
+        public static DataTable ExecuteStoreProcedure(string procedure, List<SqlParameter> param)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(strConexion))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = procedure;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Connection = conn;
+
+                    if (param != null)
+                    {
+                        foreach (SqlParameter item in param)
+                        {
+                            cmd.Parameters.Add(item);
+                        }
+                    }
+
+                    cmd.ExecuteNonQuery();
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    return dt;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static void ExecStoreProcedure(string procedure, List<SqlParameter> param)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(strConexion))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = procedure;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Connection = conn;
+
+                    if (param != null)
+                    {
+                        foreach (SqlParameter item in param)
+                        {
+                            cmd.Parameters.Add(item);
+                        }
+                    }
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
