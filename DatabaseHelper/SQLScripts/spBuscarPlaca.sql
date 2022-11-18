7828594f3c2df98a6135be4a15ff0412766ddd3d
leandrovargasvalverde <leandro9700@gmail.com>
2022-11-17 22:25:20 -0600

    Create spBuscarPlaca.sql

---
+++ DatabaseHelper/SQLScripts/spBuscarPlaca.sql
@@ -0,0 +1,13 @@
+USE [CondominiosProject]
+GO
+
+CREATE OR ALTER PROCEDURE [dbo].[spBuscarPlaca]
+@filtro Varchar(100)
+
+AS
+	BEGIN
+		SELECT * FROM [dbo].[VehiculosPorCondominos] vc WHERE vc.placa LIKE '%' + @filtro + '%'
+END
+GO
+
+EXEC spBuscarPlaca
