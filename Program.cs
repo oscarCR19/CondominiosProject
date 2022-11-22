168e1455a69381af6166e23835844032840bb74c
Jose Ureña Aguilar <urea.jose@hotmail.com>
2022-11-21 20:17:12 -0600

    Revert "Actualización Program.cs"
    This reverts commit d98acbcbfa3783cabd0520ec23c6ca495aa8d977.

--- Program.cs
+++ Program.cs
@@ -3,13 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromSeconds(10);
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,8 +19,6 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
-app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
