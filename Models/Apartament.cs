4bc2629e98ef775d3ed411334041232ae376dd41
oscarCR19 <oscarmora2006@gmail.com>
2022-11-21 22:52:20 -0600

    Modificacion del controlador

---
+++ Models/Apartament.cs
@@ -0,0 +1,11 @@
+﻿namespace Proyecto.Models
+{
+    public class Apartament
+    {
+        public int Id { get; set; }
+        public int Id_Condominium { get; set; }
+        public int Rooms { get; set; }
+        public int Parking { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
