72ed7e9c8820baf665ac65a8ddd38be287eb8bd5
oscarCR19 <oscarmora2006@gmail.com>
2022-11-21 22:51:03 -0600

    Se agregó como parte de backend la carpeta para hacer el CRUD de personas

---
+++ Persons/Persons.cs
@@ -0,0 +1,25 @@
+﻿namespace Proyecto.Persons
+{
+    public class Persons
+    {
+
+        public void InserPerson()
+        {
+            //codigo a desarrollar
+            //Llamados a databate con sus respectivos sp
+        }
+
+        public void GetPerson()
+        {
+            //Codigo a desarrollar
+            //Llamados a databate con sus respectivos sp
+        }
+
+        public void ValidatePerson()
+        {
+            //Codigo a desarrollar
+            //Llamados a databate con sus respectivos sp
+        }
+
+    }
+}
