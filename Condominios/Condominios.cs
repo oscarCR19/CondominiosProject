cddd7fbeff9f151cc253398c27ddfd154bf5b449
leandrovargasvalverde <leandro9700@gmail.com>
2022-12-10 17:03:27 -0600

    CRUD Condominios

--- Condominios/Condominios.cs
+++ Condominios/Condominios.cs
@@ -25,9 +25,7 @@ namespace Proyecto.Condominios
             {
                 return false;
             }
-
         }
-
         public static List<Condominium> GetCondominios()
         {
             DataTable ds = DatabaseHelper.DatabaseHelper.ExecuteStoreProcedure("spGetCondominios", null);
@@ -38,6 +36,7 @@ namespace Proyecto.Condominios
             {
                 condominiosList.Add(new Condominium()
                 {
+                    Id = Convert.ToInt32(dr["id_Con"]),
                     Name = dr["nombre"].ToString(),
                     Address = dr["direccion"].ToString(),
                     Phone = dr["tel"].ToString(),
@@ -45,6 +44,68 @@ namespace Proyecto.Condominios
                 });
             }
             return condominiosList;
+        }
+
+        public static List<Condominium> GetIDCondominio(int Id)
+        {
+            List<SqlParameter> param = new List<SqlParameter>()
+            {
+                new SqlParameter("@id_Con", Id)
+            };
+            DataTable ds = DatabaseHelper.DatabaseHelper.ExecuteStoreProcedure("spGetIDCondominio", param);
+
+            List<Condominium> condominio = new List<Condominium>();
+
+            foreach (DataRow dr in ds.Rows)
+            {
+                condominio.Add(new Condominium()
+                {
+                    Id = Convert.ToInt64(dr["id_Con"]),
+                    Name = dr["nombre"].ToString(),
+                    Address = dr["direccion"].ToString(),
+                    Phone = dr["tel"].ToString(),
+                    Logo = dr["logo"].ToString()
+                });
+            }
+            return condominio;
+        }
+        public static bool EditCondominio(Condominium condominio)
+        {
+            try
+            {
+                List<SqlParameter> param = new List<SqlParameter>()
+                {
+                    new SqlParameter("@id_Con", condominio.Id),
+                    new SqlParameter("@nombre", condominio.Name),
+                    new SqlParameter("@direccion", condominio.Address),
+                    new SqlParameter("@tel", condominio.Phone),
+                    new SqlParameter("@logo",condominio.Logo)
+                };
+                DatabaseHelper.DatabaseHelper.ExecuteStoreProcedure("spEditarCondominios", param);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        public static bool DeleteCondominio(int Id)
+        {
+            try
+            {
+                List<SqlParameter> param = new List<SqlParameter>()
+                {
+                    new SqlParameter("@id_Con" , Id),
+                };
+                DatabaseHelper.DatabaseHelper.ExecuteStoreProcedure("spEliminarCondominios", param);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         //Metodo para obtener los condominios de determinada empresa
