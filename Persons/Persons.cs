4e522508c6f7df766de578218d244cb5a4256a33
oscarCR19 <oscarmora2006@gmail.com>
2022-12-12 16:44:29 -0600

    Modificacion de copntroladores para implementar update personas

--- Persons/Persons.cs
+++ Persons/Persons.cs
@@ -33,21 +33,19 @@ namespace Proyecto.Persons
 
         
 
-        public static List<Person> ValidatePerson(String ced, string correo,string tel)
+        public static List<Person> ValidatePerson(string ced, string correo,string tel)
         {
             List<SqlParameter> paramList = new List<SqlParameter>()
             {
 
-                new SqlParameter("usuario",ced),
-                new SqlParameter("contra",correo),
-                new SqlParameter("usuario",tel),
+                new SqlParameter("cedula",ced),
+                new SqlParameter("correo",correo),
+                new SqlParameter("telefono",tel),
                 
 
 
             };
-
-
-            DataTable ds = DatabaseHelper.DatabaseHelper.ExecuteStoreProcedure("spValidarPersonas", paramList);
+             DataTable ds = DatabaseHelper.DatabaseHelper.ExecuteStoreProcedure("spValidarPersonas", paramList);
 
             List<Person> personList = new List<Person>();
             Person person = new Person();
@@ -61,7 +59,36 @@ namespace Proyecto.Persons
             return personList;
         }
 
-        public static Person ValidateLoginPerson(String usuario,string contra)
+
+        public static List<Person> ValidateUpdatePerson(string ced, string correo, string tel,int idPeron)
+        {
+            List<SqlParameter> paramList = new List<SqlParameter>()
+            {
+
+                new SqlParameter("cedula",ced),
+                new SqlParameter("correo",correo),
+                new SqlParameter("telefono",tel),
+                new SqlParameter("idPerson",idPeron)
+
+
+
+            };
+            DataTable ds = DatabaseHelper.DatabaseHelper.ExecuteStoreProcedure("spValidarUpdatePersonas", paramList);
+
+            List<Person> personList = new List<Person>();
+            Person person = new Person();
+
+            foreach (DataRow dr in ds.Rows)
+            {
+                person.User = dr["usuario"].ToString();
+                person.Password = dr["contra"].ToString();
+                personList.Add(person);
+            }
+            return personList;
+        }
+
+
+        public static Person ValidateLoginPerson(string usuario,string contra)
         {
             List<SqlParameter> paramList = new List<SqlParameter>()
             {
@@ -90,7 +117,7 @@ namespace Proyecto.Persons
         {
             List<SqlParameter> param = new List<SqlParameter>()
             {
-
+                new SqlParameter("id_Person",person.Id),
                 new SqlParameter("id_Rol",person.Id_Rol),
                 new SqlParameter("ced",person.Ced),
                 new SqlParameter("nombre1",person.FirstName),
@@ -99,15 +126,46 @@ namespace Proyecto.Persons
                 new SqlParameter("apellido2",person.LastName2),
                 new SqlParameter("telef",person.Phone),
                 new SqlParameter("correo",person.Email),
-                new SqlParameter("usuario",person.User),
                 new SqlParameter("contra",person.Password)
 
 
             };
 
-            DataTable ds = DatabaseHelper.DatabaseHelper.ExecuteStoreProcedure("", param);//falta sp
+            DataTable ds = DatabaseHelper.DatabaseHelper.ExecuteStoreProcedure("spEditarPersona", param);
 
 
+        }
+
+
+        public static Person GetPerson(int idPerson)
+        {
+            List<SqlParameter> paramList = new List<SqlParameter>()
+            {
+
+                new SqlParameter("idPerson",idPerson),
+                
+};
+
+
+            DataTable ds = DatabaseHelper.DatabaseHelper.ExecuteStoreProcedure("spGetPerson", paramList);
+
+            Person person = new Person();
+
+            foreach (DataRow dr in ds.Rows)
+            {
+                person.Id =Convert.ToInt32(dr["id_Person"]);
+                person.Id_Rol = Convert.ToInt32(dr["id_Rol"]);
+                person.Ced = dr["ced"].ToString();
+                person.FirstName = dr["nombre1"].ToString();
+                person.MiddleName = dr["nombre2"].ToString();
+                person.LastName1= dr["apellido1"].ToString();
+                person.LastName2 = dr["apellido2"].ToString();
+                person.Phone = dr["telef"].ToString();
+                person.Email = dr["correo"].ToString();
+                person.User= dr["usuario"].ToString();
+                person.Password = dr["contra"].ToString();
+             }
+            return person;
         }
 
 
